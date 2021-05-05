c5a880400a79f8af544e10fc877ee5e254d257a3
xiaoqing-z <xiaoqingz347@gmail.com>
2021-05-05 18:23:02 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ServicesAndSupport from "./containers/ServicesAndSupport";
+
+function App() {
+  return (
+    <div className="App">
+      <ServicesAndSupport></ServicesAndSupport>
+    </div>
+  );
+}
+
+export default App;
